34719792c725b406b7c7e4b3739d37e11f635424
Emerson Florencio <emersonflorencio@hotmail.com>
2023-04-30 14:29:20 -0300

    feat: implementando a função get_unique_industries e filter do jobs_types e industry

--- src/insights/industries.py
+++ src/insights/industries.py
@@ -1,37 +1,21 @@
 from typing import List, Dict
+from src.insights.jobs import read
 
 
 def get_unique_industries(path: str) -> List[str]:
-    """Checks all different industries and returns a list of them
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique industries
-    """
-    raise NotImplementedError
+    """Checks all different industries and returns a list of them"""
+    industries_list = read(path)
+    set_industries = set()
+    for industries in industries_list:
+        if industries["industry"] != "":
+            set_industries.add(industries["industry"])
+    return set_industries
 
 
 def filter_by_industry(jobs: List[Dict], industry: str) -> List[Dict]:
-    """Filters a list of jobs by industry
-
-    Parameters
-    ----------
-    jobs : list
-        List of jobs to be filtered
-    industry : str
-        Industry for the list filter
-
-    Returns
-    -------
-    list
-        List of jobs with provided industry
-    """
-    raise NotImplementedError
+    """Filters a list of jobs by industry"""
+    return [
+        filtered_industries
+        for filtered_industries in jobs
+        if filtered_industries["industry"] == industry
+    ]
--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -26,19 +26,9 @@ def get_unique_job_types(path: str) -> List[str]:
 
 
 def filter_by_job_type(jobs: List[Dict], job_type: str) -> List[Dict]:
-    """Filters a list of jobs by job_type
-
-    Parameters
-    ----------
-    jobs : list
-        List of jobs to be filtered
-    job_type : str
-        Job type for the list filter
-
-    Returns
-    -------
-    list
-        List of jobs with provided job_type
-    """
-
-    raise NotImplementedError
+    """Filters a list of jobs by job_type"""
+    return [
+        filtered_jobs
+        for filtered_jobs in jobs
+        if filtered_jobs["job_type"] == job_type
+    ]
